1a52f5f2faf994ff29600080c300a6ce56f79c97
David Krupp <david.krupp318@gmail.com>
2025-07-17 16:18:40 -0400

    Delete safety.py

--- safety.py
+++
@@ -1,9 +0,0 @@
-# safety.py
-import RPi.GPIO as GPIO
-
-E_STOP_PIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(E_STOP_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-def is_emergency():
-    return GPIO.input(E_STOP_PIN) == GPIO.LOW
